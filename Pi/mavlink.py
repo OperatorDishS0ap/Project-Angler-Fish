8bf4367e2ec9ac3d95082a52cd4bdb18d9d4f425
OperatorDishS0ap <theskeetfisher@gmail.com>
2026-01-29 13:03:28 -0600

    Modify mavlink.py to wait for heartbeat and print data
    Replaced autopilot reboot with heartbeat wait and added information retrieval loop.

--- Pi/mavlink.py
+++ Pi/mavlink.py
@@ -5,4 +5,12 @@ from pymavlink import mavutil
 master = mavutil.mavlink_connection("/dev/serial0", baud=115200)
 
 # Restart the ArduSub board !
-master.reboot_autopilot()
+master.wait_heartbeat()
+
+# Get some information !
+while True:
+    try:
+        print(master.recv_match().to_dict())
+    except:
+        pass
+    time.sleep(0.1)
