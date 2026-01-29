2acef458e7d58d9fe94e6108fbd57cf7d7a7521d
OperatorDishS0ap <theskeetfisher@gmail.com>
2026-01-29 13:45:37 -0600

    Update mavlink.py

--- Pi/mavlink.py
+++ Pi/mavlink.py
@@ -8,28 +8,31 @@ master = mavutil.mavlink_connection("/dev/serial0", baud=115200)
 # Restart the ArduSub board !
 master.wait_heartbeat()
 
-# Arm
-# master.arducopter_arm() or:
-master.mav.command_long_send(
-    master.target_system,
-    master.target_component,
-    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-    0,
-    1, 0, 0, 0, 0, 0, 0)
+def set_rc_channel_pwm(channel_id, pwm=1500):
+    """ Set RC channel pwm value
+    Args:
+        channel_id (TYPE): Channel ID
+        pwm (int, optional): Channel pwm value 1100-1900
+    """
+    if channel_id < 1 or channel_id > 18:
+        print("Channel does not exist.")
+        return
 
-# wait until arming confirmed (can manually check with master.motors_armed())
-print("Waiting for the vehicle to arm")
-master.motors_armed_wait()
-print('Armed!')
+    # Mavlink 2 supports up to 18 channels:
+    # https://mavlink.io/en/messages/common.html#RC_CHANNELS_OVERRIDE
+    rc_channel_values = [65535 for _ in range(18)]
+    rc_channel_values[channel_id - 1] = pwm
+    master.mav.rc_channels_override_send(
+        master.target_system,                # target_system
+        master.target_component,             # target_component
+        *rc_channel_values)                  # RC channel list, in microseconds.
 
-# Disarm
-# master.arducopter_disarm() or:
-master.mav.command_long_send(
-    master.target_system,
-    master.target_component,
-    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-    0,
-    0, 0, 0, 0, 0, 0, 0)
 
-# wait until disarming confirmed
-master.motors_disarmed_wait()
+set_rc_channel_pwm(1, 1500)
+set_rc_channel_pwm(2, 1500)
+set_rc_channel_pwm(3, 1500)
+set_rc_channel_pwm(4, 1500)
+time.sleep(1)
+set_rc_channel_pwm(1, 1600)
+time.sleep(1)
+set_rc_channel_pwm(1, 1500)
