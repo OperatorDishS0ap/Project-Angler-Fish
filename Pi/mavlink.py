b1b63529c97dd308460bad045b63e22e2a303540
OperatorDishS0ap <theskeetfisher@gmail.com>
2026-01-29 13:50:20 -0600

    Implement vehicle arming and disarming commands
    Added arming and disarming functionality for the vehicle.

--- Pi/mavlink.py
+++ Pi/mavlink.py
@@ -33,6 +33,35 @@ set_rc_channel_pwm(2, 1500)
 set_rc_channel_pwm(3, 1500)
 set_rc_channel_pwm(4, 1500)
 time.sleep(1)
+
+# Arm
+# master.arducopter_arm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    1, 0, 0, 0, 0, 0, 0)
+
+# wait until arming confirmed (can manually check with master.motors_armed())
+print("Waiting for the vehicle to arm")
+master.motors_armed_wait()
+print('Armed!')
+
 set_rc_channel_pwm(1, 1600)
+print("M1 On")
 time.sleep(1)
+print("M2 Off")
 set_rc_channel_pwm(1, 1500)
+
+# Disarm
+# master.arducopter_disarm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    0, 0, 0, 0, 0, 0, 0)
+
+# wait until disarming confirmed
+master.motors_disarmed_wait()
