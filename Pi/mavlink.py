a6edde14133f207eeb7e9f81ce1eb3ad9c0c85bc
OperatorDishS0ap <theskeetfisher@gmail.com>
2026-01-29 12:58:43 -0600

    Modify mavlink.py for connection and control commands
    Updated connection method and added arm/disarm commands.

--- Pi/mavlink.py
+++ Pi/mavlink.py
@@ -1,40 +1,38 @@
-import time
+"""
+Example of how to Arm and Disarm an Autopilot with pymavlink
+"""
 # Import mavutil
 from pymavlink import mavutil
 
-def wait_conn():
-    """
-    Sends a ping to stabilish the UDP communication and awaits for a response
-    """
-    msg = None
-    while not msg:
-        master.mav.ping_send(
-            int(time.time() * 1e6), # Unix time in microseconds
-            0, # Ping number
-            0, # Request ping of all systems
-            0 # Request ping of all components
-        )
-        msg = master.recv_match()
-        time.sleep(0.5)
-
 # Create the connection
-#  Companion is already configured to allow script connections under the port 9000
-# Note: The connection is done with 'udpout' and not 'udpin'.
-#  You can check in http:192.168.1.2:2770/mavproxy that the communication made for 9000
-#  uses a 'udp' (server) and not 'udpout' (client).
-master = mavutil.mavlink_connection('udpout:0.0.0.0:9000')
+master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
+# Wait a heartbeat before sending commands
+master.wait_heartbeat()
 
-# Send a ping to start connection and wait for any reply.
-#  This function is necessary when using 'udpout',
-#  as described before, 'udpout' connects to 'udpin',
-#  and needs to send something to allow 'udpin' to start
-#  sending data.
-wait_conn()
+# https://mavlink.io/en/messages/common.html#MAV_CMD_COMPONENT_ARM_DISARM
 
-# Get some information !
-while True:
-    try:
-        print(master.recv_match().to_dict())
-    except:
-        pass
-    time.sleep(0.1)
+# Arm
+# master.arducopter_arm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    1, 0, 0, 0, 0, 0, 0)
+
+# wait until arming confirmed (can manually check with master.motors_armed())
+print("Waiting for the vehicle to arm")
+master.motors_armed_wait()
+print('Armed!')
+
+# Disarm
+# master.arducopter_disarm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    0, 0, 0, 0, 0, 0, 0)
+
+# wait until disarming confirmed
+master.motors_disarmed_wait()
